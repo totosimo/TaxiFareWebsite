10efcc910c95e6f62e7c811b0c6c6dba011e1435
Simone Totino <totino.simone@gmail.com>
2021-05-28 16:59:29 +0300

    set-up the API call and minimal page formatting

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 run_streamlit:
-    streamlit run app.py
+	streamlit run app.py
--- app.py
+++ app.py
@@ -1,48 +1,71 @@
 import streamlit as st
+import requests
+import datetime
+
+'''# Simo's taxi fare prediction'''
+
+st.markdown('''A simple API to estimate taxi fares based on route and time indications''')
 
 '''
-# TaxiFareModel front
+## Provide simobot with the details of your route 🤔 
 '''
+st.text("")
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
+date = st.date_input('Please insert your date')
+time = st.time_input('Please insert your date and time')
+pickup_datetime = f'{date} {time}'
+pickup_lon = st.text_input('Enter your pickup longitude', '40.7614327')
+pickup_lat = st.text_input('Enter your pickup latitude', '-73.9798156')
+dropoff_lon = st.text_input('Enter your dropoff longitude', '40.6513111')
+dropoff_lat = st.text_input('Enter your dropoff latitude', '-73.8803331')
+passenger_count = st.text_input('Enter the amount of passengers', '2')
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
 
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
+# test_url = 'https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2'
 url = 'https://taxifare.lewagon.ai/predict'
 
-if url == 'https://taxifare.lewagon.ai/predict':
+# if url == 'https://taxifare.lewagon.ai/predict':
+#     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+# 2. Let's build a dictionary containing the parameters for our API...
+
+user_params = dict(
+    pickup_datetime=pickup_datetime,
+    pickup_longitude=pickup_lon,
+    pickup_latitude=pickup_lat,
+    dropoff_longitude=dropoff_lon,
+    dropoff_latitude=dropoff_lat,    
+    passenger_count=passenger_count
+    )
+
+# check if params assignment works
+# st.write(user_params)
+
+# 3. Let's call our API using the `requests` package...
+
+response = requests.get(
+    url,
+    params=user_params
+)
+prediction = round(response.json()['prediction'],1)
+# response = requests.get(
+#     test_url
+# )
 
 '''
 
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
 '''
+# if response.status_code == 200:
+#     st.write("API call success")
+# else:
+#     st.write("API call error")
+# st.write(response.json())
+
+st.text("")
+
+if st.button('predict'):
+    # print is visible in server output, not in the page
+    # st.write('I was clicked 🎉')
+    # st.write(prediction)
+    
+    st.write(f"##🎉 simoBot's prediction: {prediction} $")
